1d535d41ed93b5808e15c5eeb3f4d42cd9c51666
Sejun Park <qkrtpwns1999@naver.com>
2021-05-10 16:09:20 +0900

    프래그먼트별 Toolbar Menu를 다르게 힘 SheetListFragment Toolbar Menu 추가 MainActivity에 있던 Menu 삭제

--- app/src/main/java/com/LaCucaracha/musible/main/MainActivity.java
+++ app/src/main/java/com/LaCucaracha/musible/main/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -25,36 +27,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        NavController navController=Navigation.findNavController(this,R.id.nav_host_fragment);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//               navController.navigate(R.id.sheetDetailFragment);
-//            }
-//        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
